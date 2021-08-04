55d50c48a06c462db126968fe8099058b39840ef
Brian W. Wolter <brian@brianwolter.com>
2021-08-03 23:08:22 -0400

    Socket based on key param

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::fs;
+use std::env;
 use std::sync::Arc;
 
+use tokio::io;
 use tokio::net::UnixListener;
 use tokio::sync::mpsc::Sender;
 
@@ -9,36 +12,39 @@ use multiread::server::Server;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-  let listener = UnixListener::bind("__multiread").unwrap();
+  let mut args = env::args();
+  if args.len() < 2 {
+    println!("*** Usage: multiread <key>");
+    return Ok(());
+  }
+  
+  let key = args.nth(1).unwrap();
+  let mut dir = env::temp_dir();
+  dir.push("multiread");
+
+  let dup = dir.clone();
+  fs::create_dir_all(dup).expect("Could not create socket path");
+  
+  dir.push(key);
+  println!(">>> Bind: {}", dir.display());
+  
+  let listener = match UnixListener::bind("__multiread") {
+    Ok(listener) => listener,
+    Err(ref err) if err.kind() == io::ErrorKind::AddrInUse => {
+      println!(">>> CLIENT PATH");
+      return Ok(());
+    }
+    Err(err) => {
+      println!("*** Could not bind: {:?}", err);
+      return Ok(());
+    }
+  };
+  
   let streams: DashMap<String, Sender<String>> = DashMap::new();
   let clients = Arc::new(streams);
   
   let server = Server::new(clients);
   server.run(listener);
-  
-  // {
-  //   let clients = Arc::clone(&clients);
-  //   thread::spawn(move || handle_server(rx, Arc::clone(&clients)));
-  // }
-  
-  // let mut i = 0;
-  // loop {
-  //   match listener.accept().await {
-  //     Ok((stream, _addr)) => {
-  //       let dup = tx.clone();
-  //       let (vtx, vrx) = mpsc::channel(2);
-  //       let clients = Arc::clone(&clients);
-  //       clients.insert(format!("{}", i), vtx);
-  //       let client = Client::new(format!("{}", i), stream, clients);
-  //       thread::spawn(move || client.handle(dup, vrx));
-  //       i = i + 1;
-  //     }
-  //     Err(err) => {
-  //       println!("Couldn't connect: {:?}", err);
-  //       break;
-  //     }
-  //   }
-  // }
   
   Ok(())
 }
